d6cd2acec11a611c4410ec4a5ff1818acfcd388f
Divad001 <gyoredavid1993@gmail.com>
2020-08-19 12:32:47 +0200

    added dual lang v1

--- src/main/java/com/mypage/app/controllers/MainController.java
+++ src/main/java/com/mypage/app/controllers/MainController.java
@@ -1,23 +1,34 @@
 package com.mypage.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String index(@RequestParam(required = false) String lang) {
+        if (lang != null && lang.equals("EN")) {
+            return "EN/indexEN";
+        }
+        return "HU/index";
     }
 
     @GetMapping("/studies")
-    public String studies() {
-        return "studies";
+    public String studies(@RequestParam(required = false) String lang) {
+        if (lang != null && lang.equals("EN")) {
+            return "EN/studiesEN";
+        }
+        return "HU/studies";
     }
 
     @GetMapping("/contact")
-    public String contact() {
-        return "contact";
+    public String contact(@RequestParam(required = false) String lang) {
+        if (lang != null && lang.equals("EN")) {
+            return "EN/contactEN";
+        }
+        return "HU/contact";
     }
 }
